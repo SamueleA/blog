144e53d3e0897a2e9eac63e0f9324fa0341a865f
sagostinelli <batonenbois@gmail.com>
2020-02-12 13:39:06 -0500

    test3

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -31,7 +31,7 @@ export function Bio() {
               }}
             />
             <p>
-              Learn about test2, Blockchain and general software engineering tips 😉.
+              Learn about test3, Blockchain and general software engineering tips 😉.
             </p>
           </div>
         )
